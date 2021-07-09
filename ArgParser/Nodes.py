7a2e78e7ff228c842b8ef07e32aeadc64f930d65
Binary Storm <gerick.de.kruyf123@gmail.com>
2021-07-09 20:18:21 +0200

    Added nodes

---
+++ ArgParser/Nodes.py
@@ -0,0 +1,45 @@
+
+class ArgNode:
+  def __init__(self, tokan):
+    self.val = tokan.VAL
+
+  def __repr__(self):
+    return "{}".format(self.val)
+
+
+class FlagNode:
+  def __init__(self, tokan, arguments):
+    self.val = tokan.VAL
+    self.args = arguments
+
+  def __repr__(self):
+    ret = "Val: {} <".format(self.val)
+    for i in self.args:
+      ret += "{}".format(i)
+    ret += ">"
+    return ret
+
+
+class CmdNode:
+  def __init__(self, tokan, arguments):
+    self.val = tokan.VAL
+    self.args = arguments
+
+  def __repr__(self):
+    ret = "Val: {} <".format(self.val)
+    for i in self.args:
+      ret += "{}".format(i)
+    ret += ">"
+    return ret
+
+if __name__ == "__main__":
+  from LexerClass import Lexer
+  l = Lexer()
+  cmd, flag, arg = l.gen_tokan_stream("open -f \"/home\"")
+  arg = ArgNode(arg)
+  arg = [arg]
+  flag = FlagNode(flag, arg)
+  flag = [flag]
+  cmd = CmdNode(cmd, flag)
+  print(cmd)
+
